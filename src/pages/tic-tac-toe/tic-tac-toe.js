4e87c341433f6ff513c63eea14b03bb552c5c7b3
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-17 09:21:13 +0100

    A

--- src/pages/tic-tac-toe/tic-tac-toe.js
+++ src/pages/tic-tac-toe/tic-tac-toe.js
@@ -1,9 +1,42 @@
-player1 = 'Hafedh';
-player2 = 'Hafedh';
+player1 = "Hafedh";
+player2 = "Hafedh";
 
-start = false; 
+grid = Array(9);
+
+function load() {
+  for (indx = 0; indx < 9; indx++) {
+    grid[indx] = document.getElementById(`${indx + 1}`);
+  }
+  console.log(JSON.stringify(grid));
+}
+
+document.addEventListener("DOMContentLoaded", load);
+
+turn = false;
+
+function drawLine(startX, startY, endX, endY) {
+  const line = document.getElementById('line');
+
+  // Calculate distance and angle between two points
+  const distance = Math.sqrt((endX - startX) ** 2 + (endY - startY) ** 2);
+  const angle = Math.atan2(endY - startY, endX - startX) * (180 / Math.PI);
+
+  // Set line length, angle, and position
+  line.style.width = distance + 'px';
+  line.style.transform = `rotate(${angle}deg)`;
+  line.style.left = startX + 'px';
+  line.style.top = startY + 'px';
+}
 
 function drawPlayer(id = 0) {
-    start = !start;
-    document.getElementById(String(id)).innerHTML = "<img class='duration-700 hover:scale-120' src='../../../assets/images/o.png'></img>";
+  if (!grid[id - 1].innerHTML) {
+    console.log(grid[id - 1].style.position);
+    turn = !turn;
+    grid[
+      id - 1
+    ].innerHTML = `<img class='duration-700 hover:scale-110' src='../../../assets/images/${
+      turn ? "x" : "o"
+    }.png'></img>`;
+  }
 }
+
