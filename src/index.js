ae055f83ca0eec6f07fef852a85b558801df481f
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-24 10:14:55 +0100

    ModifiedM src/index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const preloader = document.querySelector("[data-preaload]");
 
@@ -7,41 +7,42 @@ window.addEventListener("load", function () {
   document.body.classList.add("loaded");
 });
 
-const addEventOnElements = function(elements, eventType, callback) {
-  for(let i = 0, len = elements.length ; i < len ; i += 1){
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i += 1) {
     elements[i].addEventListener(eventType, callback);
   }
-}
+};
 
-const navbar = document.querySelector('[data-navbar]');
-const navTogglers = document.querySelectorAll('[data-nav-toggler]');
-const overlay = document.querySelector('[data-overlay]');
-const togglNavbar = function(){
-  navbar.classList.toggle('active');
-  overlay.classList.toggle('active');
-  document.body.classList.toggle('nav-active');
-}
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+const togglNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+};
 
-addEventOnElements(navTogglers, 'click', togglNavbar);
+addEventOnElements(navTogglers, "click", togglNavbar);
 
-const header = document.querySelector('[data-header]');
+const header = document.querySelector("[data-header]");
 
 let lastScrollPos = 0;
 
-const hideHeader = function() {
+const hideHeader = function () {
   const isScrollBottom = lastScrollPos < window.scrollY;
-  if(isScrollBottom){
-    header.classList.add('hide');
-  }else{
-    header.classList.remove('hide');
+  if (isScrollBottom) {
+    header.classList.add("hide");
+  } else {
+    header.classList.remove("hide");
   }
-}
+  
+  lastScrollPos = window.scrollY;
+};
 
-window.addEventListener('scroll', function(){
-  if(this.window.scrollY >= 50){
-    header.classList.add('active');
-  }else {
-    header.classList.remove('active');
+window.addEventListener("scroll", function () {
+  if (this.window.scrollY >= 50) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
   }
-},
-);
+});
