acdc7b513ddc10264b5699149959cc25b0f0615a
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-24 10:09:53 +0100

    ModifiedM src/index.js

--- src/index.js
+++ src/index.js
@@ -34,5 +34,8 @@ const hideHeader = function() {
 window.addEventListener('scroll', function(){
   if(this.window.scrollY >= 50){
     header.classList.add('active');
+  }else {
+    header.classList.remove('active');
   }
-});
+},
+);
