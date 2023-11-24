b82a936d73f0a954bcb1d75e4da66b190782987a
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-24 09:48:08 +0100

    ModifiedM src/index.js

--- src/index.js
+++ src/index.js
@@ -16,3 +16,6 @@ const addEventOnElements = function(elements, eventType, callback) {
 const navbar = document.querySelector('[data-navbar]');
 const navTogglers = document.querySelector('[data-nav-toggler]');
 const overlay = document.querySelector('[data-overlay]');
+const togglNavbar = function(){
+  navbar.classList.toggle
+}
